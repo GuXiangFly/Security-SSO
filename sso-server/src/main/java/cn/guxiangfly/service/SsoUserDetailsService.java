ae2186afe9aab31ec164f1e3ebae6b83a1bd939f
guxiangfly <964999133@qq.com>
2017-10-23 21:04:34 +0800

    一些修改

--- sso-server/src/main/java/cn/guxiangfly/service/SsoUserDetailsService.java
+++ sso-server/src/main/java/cn/guxiangfly/service/SsoUserDetailsService.java
@@ -1,10 +1,12 @@
 package cn.guxiangfly.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 /**
@@ -15,9 +17,14 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class SsoUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return new User(username,
-                "",AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
+                passwordEncoder.encode("123456"),
+                AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER"));
     }
 }
